83da5a0d5b34692ae1f06f7046085cfcf284b629
suzandsouza <89693202+suzandsouza@users.noreply.github.com>
2021-09-06 12:08:26 +0530

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,22 @@
+import random
+def play():
+    user=input("Type 'r' for rock, 'p' for paper, 's' for scissors")
+    computer= random.choice(['r','p','s'])
+
+    if user == computer:
+        return 'It\'s a tie'
+
+    #r>s, s>p, p>r
+    
+    def is_win(player,opponent):
+        #return true if player wins
+        #r>s, s>p p>r
+        if(player=='r' and opponent=='s') or (player=='s' and opponent=='p') or (player=='p' and opponent=='r'):
+            return True
+
+    if is_win(user,computer):
+        return 'You won!'
+    return 'You Lost!'
+print(play())
+print(play())
+print(play())
